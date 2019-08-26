9b7dbb75e048db48e490d0beb2a9147d0f7d8706
15738383930 <15738383930@163.com>
2019-08-26 18:34:12 +0800

    springboot集成了redis； redis配置及工具类的实现； 代码优化。

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+/**
+ * 项目启动类
+ * 1. 因项目用到redis数据库，且没有配置该数据源（没必要），故，排除DataSourceAutoConfiguration.class
+ * 2. 因项目需要打war包（已排除内嵌Tomcat）进行平台移植发布，故，继承SpringBootServletInitializer类，实现等同于web.xml的功能
+ * @author zhou
+ *
+ */
+@SpringBootApplication(exclude =DataSourceAutoConfiguration.class )
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -7,11 +9,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.utils.RedisUtil;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	// redis中存储的过期时间60s
+    private static int ExpireTime = 60;   
+    
+    public DemoApplicationTests() {
+		System.out.println("Redis数据库正在初始化。。。");
+	}
+
+    @Resource
+    private RedisUtil redisUtil;
+    
 	@Test
 	public void contextLoads() {
         Logger logger = LoggerFactory.getLogger(getClass());     
@@ -20,5 +34,20 @@ public class DemoApplicationTests {
         logger.debug("debug ...");
         logger.info("info ...");
         logger.trace("trace ...");
+	}
+	
+	@Test
+	public void a() {
+		final String key = "username";
+		System.out.println("Redis数据库初始化成功。");
+		System.out.println("Redis正在进行存储：" + key);
+		if (redisUtil.set(key, 2645244266l)) {
+			System.out.println(key + "存储成功！");
+		}
+		if (redisUtil.hasKey(key)) {
+			System.out.println(key + "的值是：" + redisUtil.get(key));
+		} else {
+			System.out.println(key + "不存在！");
+		}
 	}
 }
